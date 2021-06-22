0e32c1d888d55f3db839c30178606da283b240cc
Gary Darnell <gdarnell9@gmail.com>
2021-06-22 17:32:25 +0100

    Sixth solo test passed.

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -79,5 +79,24 @@ describe('Calculator', () => {
     button2.simulate('click');
     button1.simulate('click');
     expect(runningTotal.text()).toEqual('21');
+  });
+
+  it('should be able to chain multiple operations together', () => {
+    const button2 = container.find('#number2');
+    const button1 = container.find('#number1');
+    const buttonDivide = container.find('#operator-divide');
+    const button7 = container.find('#number7');
+    const buttonMultiply = container.find('#operator-multiply');
+    const button5 = container.find('#number5');
+    const buttonEquals = container.find('#operator-equals')
+    const runningTotal = container.find('#running-total');
+    button2.simulate('click');
+    button1.simulate('click');
+    buttonDivide.simulate('click');
+    button7.simulate('click');
+    buttonMultiply.simulate('click');
+    button5.simulate('click');
+    buttonEquals.simulate('click');
+    expect(runningTotal.text()).toEqual('15');
   })
 })
